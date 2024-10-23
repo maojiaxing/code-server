832cf11bac9be1ea5e71c18003035676421d71a4
maojiaxing <maojiaxing.s@gmail.com>
2024-10-23 16:01:51 +0000

    add flash.nvim integration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,20 @@
-aa
+function bootstrap(url, ref) 
+    local name = url:gsub(".*/", "")
+    local path = vim.fn.stdpath("data") .. "/site/pack/bootstrap/start/" .. name
+
+    if vim.fn.isdirectory(path) == 0 then
+        print(name .. ": installing in data dir...")
+
+        vim.fn.system {"git", "clone", url, path}
+
+        if ref then
+            vim.fn.system {"git", "-C", path, "checkout", ref}
+        end
+
+        vim.cmd "redraw"
+        print(name .. ": finished installing")
+    end
+end
+
+-- flash.nvim
+bootstrap("https://github.com/folke/flash.nvim.git", ref="v2.1.0")
